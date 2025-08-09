30bfebc352b21d12e899b965a8c3dc5f09ee274b
prathipa <prathipakaruppaiya@gmail.com>
2025-08-09 12:50:28 +0530

    third commit

---
+++ bit.java
@@ -0,0 +1,12 @@
+public class wild{
+public static void main(String[] args){
+String userName = "Prathipa";
+int stepsWalked = 8500;
+double calories = 0.8500 ;
+
+System.out.println("User: "+ userName );
+System.out.println("Steps Walked:"+stepsWalked);
+System.out.println("Calories Burned:"+calories);
+
+}
+}
